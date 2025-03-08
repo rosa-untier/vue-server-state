c2328f4fdfa2433191d9041f206ee50914fa60d5
vinpogo <vgrossmayer@gmail.com>
2025-03-08 13:19:46 +0100

    WIP: add actual check if getter should be called

--- src/pinged.ts
+++ src/pinged.ts
@@ -1,13 +1,20 @@
 import { type Ref, ref } from "vue";
 
-type PingOptions = {
+export type PingOptions<Data> = {
   time?: number;
   retries?: number;
   autoStart?: boolean;
+  needsUpdate?: (data?: Data) => Promise<boolean>;
 };
+
 export function pinged<Data>(
   getter: () => Promise<Data>,
-  { time = 1000, retries = 3, autoStart = true }: PingOptions = {},
+  {
+    time = 1000,
+    retries = 3,
+    autoStart = true,
+    needsUpdate = (_?: Data) => Promise.resolve(true),
+  }: PingOptions<Data> = {},
 ) {
   const data = ref<Data>();
   const timerId = ref(-1);
@@ -16,34 +23,40 @@ export function pinged<Data>(
     data.value = await getter();
   };
 
-  const cancel = () => clearInterval(timerId.value);
+  const stop = () => clearTimeout(timerId.value);
 
   const start = () => {
-    callInterval(setter, { time, retries }, timerId);
+    callInterval(setter, timerId, {
+      time,
+      retries,
+      needsUpdate: () => needsUpdate(data.value),
+    });
   };
 
   if (autoStart) {
     start();
   }
 
-  return { data, cancel, start };
+  return { data, stop, start };
 }
 
 function callInterval<Data>(
   cb: () => Promise<Data>,
-  opts: Pick<Required<PingOptions>, "time" | "retries">,
   timerId: Ref<number>,
+  opts: Pick<Required<PingOptions<Data>>, "time" | "retries" | "needsUpdate">,
 ) {
   let tried = 0;
   timerId.value = setTimeout(async () => {
     while (tried < opts.retries) {
       try {
-        await cb();
+        if (await opts.needsUpdate()) {
+          await cb();
+        }
         tried = Infinity;
       } catch (_) {
         tried++;
       }
     }
-    callInterval(cb, opts, timerId);
+    callInterval(cb, timerId, opts);
   }, opts.time);
 }
